fe1a27118543b8cbb02aa3ac7c1a91a7eced3396
Tobi <tobi.daniel2018@gmail.com>
2021-03-15 12:12:54 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // ater the component has mounted, make an API call
+    axios({
+      url: "https://api.themoviedb.org/3/discover/movie",
+      params: {
+        api_key: "8afdd130b22be86a60cb4c3e8b56a739",
+        language: "en-US",
+        sort_by: "popularity.desc",
+        include_adult: "false",
+        include_video: "false",
+        page: 1,
+        primary_release_year: 1999,
+      },
+    }).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>HackFlix</h1>
       </header>
+      <div className="catalogue">
+        {movies.map((movie) => {
+          return (
+            <div className="movie" key={movie.id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={`Poster for ${movie.original_title}`}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
